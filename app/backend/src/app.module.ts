f6adb377e8b9cab8f8f77969611617c9b8fa70c0
Vinicius Gomes <viniplay0599@gmail.com>
2022-11-14 20:00:01 -0300

    config(backend): connecting with database

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
