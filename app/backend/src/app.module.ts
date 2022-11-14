e0124451c45a494ead9d102f4ca0526ab8e30666
Vinicius Gomes <viniplay0599@gmail.com>
2022-11-14 20:57:00 -0300

    feat: create module, service, controller for transaction entity

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './app/user/user.module';
 import { AccountModule } from './app/account/account.module';
+import { TransactionModule } from './app/transaction/transaction.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { AccountModule } from './app/account/account.module';
     }),
     UserModule,
     AccountModule,
+    TransactionModule,
   ],
 })
 export class AppModule {}
