90b48a5770c305b8d162f99fd89d023b4eea44d5
Vinicius Gomes <viniplay0599@gmail.com>
2022-11-15 13:25:30 -0300

    feat: add field validations

--- app/backend/src/app/user/user.entity.ts
+++ app/backend/src/app/user/user.entity.ts
@@ -8,15 +8,19 @@ import {
 } from 'typeorm';
 import { hashSync } from 'bcrypt';
 import { AccountEntity } from '../account/account.entity';
+import { MinLength, Matches, IsString } from 'class-validator';
 
 @Entity({ name: 'users' })
 export class UserEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ nullable: false })
+  @IsString()
+  @MinLength(3)
+  @Column({ nullable: false, unique: true })
   username: string;
 
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/)
   @Column({ nullable: false })
   password: string;
 
