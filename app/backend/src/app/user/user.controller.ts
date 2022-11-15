a48e3979ca0950efe553a91876db99ce7c2d7a01
Vinicius Gomes <viniplay0599@gmail.com>
2022-11-15 15:55:32 -0300

    feat: implements user controller

--- app/backend/src/app/user/user.controller.ts
+++ app/backend/src/app/user/user.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserEntity } from './user.entity';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() user: UserEntity) {
+    return await this.userService.create(user);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.userService.findOne({ where: { id } });
+  }
+
+  @Get(':username')
+  async findOneByUsername(@Param('username') username: string) {
+    return await this.userService.findOne({ where: { username } });
+  }
+
+  @Put(':id')
+  async update(@Param('id', new ParseUUIDPipe()) id: string, data: UserEntity) {
+    return await this.userService.update(id, data);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.userService.delete(id);
+  }
+}
