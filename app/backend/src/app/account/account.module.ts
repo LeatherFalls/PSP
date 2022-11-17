b89d4f1d2377544501497fc7764b99e3c6ac4326
Vinicius Gomes <viniplay0599@gmail.com>
2022-11-17 14:36:14 -0300

    feat: implements get balance endpoint

--- app/backend/src/app/account/account.module.ts
+++ app/backend/src/app/account/account.module.ts
@@ -3,9 +3,11 @@ import { AccountService } from './account.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountEntity } from './account.entity';
 import { UserEntity } from '../user/user.entity';
+import { AccountController } from './account.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AccountEntity, UserEntity])],
   providers: [AccountService],
+  controllers: [AccountController],
 })
 export class AccountModule {}
